e5e7a9141403b1b38a132dafaf7321ce44274eef
marta <corcolesvallemarta@gmail.com>
2022-09-08 20:10:36 +0200

    Method to get room type

--- src/es/viajeselcaminito/Main.java
+++ src/es/viajeselcaminito/Main.java
@@ -1,8 +1,26 @@
 package es.viajeselcaminito;
 
+import es.viajeselcaminito.utilities.ResortHotels;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+
+            // Get all information from Resort Hotels provider
+            String resortHotels = ResortHotels.getAllFromResort();
+            System.out.println(resortHotels);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
