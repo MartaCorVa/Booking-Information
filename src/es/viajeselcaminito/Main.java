538530f95a5a38cd88a25894ec72b4431b8c210d
marta <corcolesvallemarta@gmail.com>
2022-09-10 17:15:03 +0200

    Get all the hotels and print the json

--- src/es/viajeselcaminito/Main.java
+++ src/es/viajeselcaminito/Main.java
@@ -1,9 +1,15 @@
 package es.viajeselcaminito;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import es.viajeselcaminito.generic.HotelGeneric;
+import es.viajeselcaminito.generic.HotelListGeneric;
+import es.viajeselcaminito.utilities.Atalaya;
 import es.viajeselcaminito.utilities.ResortHotels;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -12,8 +18,22 @@ public class Main {
         try {
 
             // Get all information from Resort Hotels provider
-            String resortHotels = ResortHotels.getAllFromResort();
-            System.out.println(resortHotels);
+            ArrayList<HotelGeneric> resortHotels = ResortHotels.getAllFromResort();
+
+            // Get all information from Atalaya provider
+            ArrayList<HotelGeneric> atalayaHotels = Atalaya.getAllFromAtalaya();
+
+            ArrayList<HotelGeneric> hotels = new ArrayList<>();
+            hotels.addAll(resortHotels);
+            hotels.addAll(atalayaHotels);
+
+            HotelListGeneric hotelListGeneric = new HotelListGeneric(hotels.toArray(new HotelGeneric[0]));
+
+            Gson prettyHotels = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create();
+
+            System.out.println(prettyHotels.toJson(hotelListGeneric));
 
 
         } catch (MalformedURLException e) {
